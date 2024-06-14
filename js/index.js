88214473a9d230f2e9246119673c10f749279519
mamanimartinpablo <mamanimartinpablo@gmail.com>
2024-06-13 23:30:09 -0300

    Agregando mas audios y algunas validaciones

--- js/index.js
+++ js/index.js
@@ -4,8 +4,6 @@ const inputSubmit = document.querySelector("#enviar")
 
 const inputText = document.querySelector("#palabra")
 
-const res = []
-
 
 inputSubmit.addEventListener('click', function(e){
     e.preventDefault()
@@ -17,11 +15,47 @@ function getData(p){
 fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${p}`) 
     .then(response => response.json()) 
     .then(data => {console.log(data)
-        let cad=`
-        <p>Como se pronuncia: ${data[0].phonetics[1].text}</p>
-        <audio src="${data[0].phonetics[0].audio}" controls>
-        </audio>
-        `
+        let cad
+        if (data.hasOwnProperty("message") || data[0].phonetics.length == 0) {
+            cad=`
+            <p>No se encontro la palabra</p>
+            `
+        }
+        else{
+            if (data[0].phonetics[0].hasOwnProperty("sourceUrl")) {
+                cad=`
+                <p>Origen : Australia</p>
+            <audio src="${data[0].phonetics[0].audio}" controls>
+            </audio>
+            `
+            if (data[0].phonetics[0].text) {
+                cad = cad + ` 
+                <p>Se pronuncia como: ${data[0].phonetics[1].text}</p>
+                `
+            }
+            cad = cad + ` 
+            `
+            }else{
+                if (data[0].phonetics[1].hasOwnProperty("sourceUrl")) {
+                    cad=`
+                    <p>Origen : Reino Unido</p>
+                <audio src="${data[0].phonetics[1].audio}" controls>
+                </audio>
+                <p>Se pronuncia como: ${data[0].phonetics[1]?.text}</p>
+                `
+                }else{
+                    if (data[0].phonetics[2].hasOwnProperty("sourceUrl")) {
+                        cad=`
+                        <p>Origen : EEUU</p>
+                    <audio src="${data[0].phonetics[2].audio}" controls>
+                    </audio>
+                    <p>Se pronuncia como: ${data[0].phonetics[1]?.text}</p>
+                    `
+                    }
+                }
+            }
+        
+        }
         document.querySelector(".container").innerHTML=cad
     }
 
